5a925cdc0d83b1fa402e1ad4497960b4ebe84c37
charlesmoreira <Onre1515>
2020-03-20 20:57:18 -0300

    :zap: Adicionado card empty com nome.

--- app/src/main/java/com/concrete/magicthegathering/feature/detailcard/presentation/ui/DetailCardFragment.kt
+++ app/src/main/java/com/concrete/magicthegathering/feature/detailcard/presentation/ui/DetailCardFragment.kt
@@ -1,5 +1,6 @@
 package com.concrete.magicthegathering.feature.detailcard.presentation.ui
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -7,6 +8,10 @@ import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.concrete.magicthegathering.R
 import com.concrete.magicthegathering.data.model.entity.favorite.CardFavorite
 import com.concrete.magicthegathering.feature.detailcard.presentation.viewmodel.DetailCardViewModel
@@ -40,10 +45,21 @@ class DetailCardFragment : Fragment(R.layout.fragment_detail_card), IDetailCardF
     }
 
     private fun loadImageCard(){
+        text_name_detail_card.text = args.nameCard
+
         Glide.with(this)
             .load(args.imageCard)
-            .placeholder(R.drawable.image_not_found)
-            .error(R.drawable.image_not_found)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                    text_name_detail_card.visibility = View.VISIBLE
+                    return false
+                }
+                override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                    text_name_detail_card.visibility = View.GONE
+                    return false
+                }
+            })
+            .placeholder(R.drawable.card_empty)
             .into(image_detail_card)
     }
 
